74f81acdf54cc02ae81dfd2ecf2c9f78d9f2570b
laure Fontanie <fontanie.laure@hotmail.fr>
2023-11-17 15:16:48 +0100

    first push

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  root "lists#index"
   resources :lists, only: %i[index show new create] do
     resources :bookmarks, only: %i[index show new create]
     # resources :movies, only: %i[:index, :show]
